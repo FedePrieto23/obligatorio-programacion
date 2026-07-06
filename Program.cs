0fe1232ef3a6f0b1f238892cbd5a1089d4dad35e
FedePrieto23 <bacholonunez@gmail.com>
2026-07-06 00:44:14 -0300

    Evitar migraciones automaticas en Render

--- Program.cs
+++ Program.cs
@@ -56,7 +56,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-    db.Database.Migrate();
+    //db.Database.Migrate();
 
     if (!db.Oficios.Any())
     {
